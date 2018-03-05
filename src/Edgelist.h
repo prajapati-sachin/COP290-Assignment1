f0b19f7157e2bc31c15604f1f5659515afd16fab
pranavbhagat5 <pranavbhagat5@outlook.com>
2018-03-05 19:04:55 +0530

    added Edgelist

---
+++ src/Edgelist.h
@@ -0,0 +1,18 @@
+#ifndef EdgeList
+#define EdgeList
+
+#include <vector>
+#include Solid_struct
+
+using namespace std;
+
+/** EdgeList :- a list of edges **/
+class EdgeList{
+	vector<edge3d> E;
+
+	//function for adding an edge to edgelist
+	void addEdge<edge3d>;
+
+	//function to delete an edge from an edgelist
+	void removeEdge<edge3d>;
+}
