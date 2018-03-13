55b419517fb3e2ad4e83711ec201a4be65dca771
pranavbhagat5 <pranavbhagat5@outlook.com>
2018-03-13 18:50:59 +0530

    added Hiddenline.h

---
+++ src/HiddenLines.h
@@ -0,0 +1,14 @@
+#ifndef HIDDEN_LINE
+#define HIDDEN_LINE
+
+#include <vector>
+#include "Vertex2d_List.h"
+#include "Edge2d_List.h"
+#include "Vertex3d_List.h"
+#include "Edge3d_List.h"
+#include "Surface3d_List.h"
+#include "Face3d_List.h"
+
+bool check_on_same_side_of_surface(Surface3d surface,Vertex3d vertex1,Vertex3d vertex2)
+
+
