b24a70b51dd95c56bdf7953a93d0d3ded3b39608
sachin-prajapati <sachinkp1211@gmail.com>
2018-03-31 23:29:21 +0530

    Added Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include "../include/Edge3d_List.h"
+#include "../include/Edge2d_List.h"
+#include "../include/HiddenLines.h"
+#include "../include/Projection2d.h"
+#include "../include/Solid3d.h"
+#include "../include/Surface3d_List.h"
+#include "../include/Vertex2d_List.h"
+#include "../include/Vertex3d_List.h"
 
 using namespace std;
 
